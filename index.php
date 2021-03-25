9cd19cf43ec45775096c3e62aaccc8277a448ef9
ZsoltAntal9999 <70635319+ZsoltAntal9999@users.noreply.github.com>
2021-03-25 22:53:37 +0100

    quiz

--- index.php
+++ index.php
@@ -34,7 +34,7 @@ ob_start();
                     <ul class="navbar-nav ml-auto">
                         <li class="nav-item mx-0 mx-lg-1"><a class="nav-link py-3 px-0 px-lg-3 rounded js-scroll-trigger" href="index.php#kontinensek">Kontinensek</a></li>
                         <li class="nav-item mx-0 mx-lg-1"><a class="nav-link py-3 px-0 px-lg-3 rounded js-scroll-trigger" href="#about">A zászlókról</a></li>
-                        <li class="nav-item mx-0 mx-lg-1"><a class="nav-link py-3 px-0 px-lg-3 rounded js-scroll-trigger" href="#contact">Contact</a></li>
+                        <li class="nav-item mx-0 mx-lg-1"><a class="nav-link py-3 px-0 px-lg-3 rounded js-scroll-trigger" href="#contact">Véleménye</a></li>
                     </ul>
                 </div>
             </div>
@@ -97,7 +97,7 @@ ob_start();
         <section class="page-section" id="contact">
             <div class="container">
                 <!-- Contact Section Heading-->
-                <h2 class="page-section-heading text-center text-uppercase text-secondary mb-0">Contact Me</h2>
+                <h2 class="page-section-heading text-center text-uppercase text-secondary mb-0">Írjon valamit nekünk!</h2>
                 <!-- Icon Divider-->
                 <div class="divider-custom">
                     <div class="divider-custom-line"></div>
@@ -117,35 +117,35 @@ ob_start();
                         header('Location: index.php');
                     }
                 ?>
-                <div>Email: <?=$email?></div>
+                <div class="pb-2">Email: <?=$email?></div>
                     <div class="col-lg-8 mx-auto">
                         <!-- To configure the contact form email address, go to mail/contact_me.php and update the email address in the PHP file on line 19.-->
                         <form class="form" method="Post">
                             
-                                <div class="form-group floating-label-form-group controls mb-0 pb-2">
-                                    <label>Name</label>
-                                    <input class="form-control" name="nev" id="name" type="text" placeholder="Name" required="required" />
+                                <div class="form-group  controls mb-0 pb-2">
+                                    <label>Név</label>
+                                    <input class="form-control" name="nev" id="name" type="text" placeholder="Név" required="required" />
                                     <p class="help-block text-danger"></p>
                                 </div>
                     
                             
-                                <div class="form-group floating-label-form-group controls mb-0 pb-2">
-                                    <label>Email Address</label>
-                                    <input class="form-control" name="mail" id="email" type="email" placeholder="Email Address" required="required"  />
+                                <div class="form-group  controls mb-0 pb-2">
+                                    <label>Email cím</label>
+                                    <input class="form-control" name="mail" id="email" type="email" placeholder="Email cím" required="required"  />
                                     <p class="help-block text-danger"></p>
                                 </div>
                             
                             
-                                <div class="form-group floating-label-form-group controls mb-0 pb-2">
-                                    <label>Phone Number</label>
-                                    <input class="form-control" id="phone" type="tel" placeholder="Phone Number" required="required"  />
+                                <div class="form-group  controls mb-0 pb-2">
+                                    <label>Telefonszám</label>
+                                    <input class="form-control" id="phone" type="tel" placeholder="Telefonszám"/>
                                     <p class="help-block text-danger"></p>
                                 </div>
                             
                             
-                                <div class="form-group floating-label-form-group controls mb-0 pb-2">
-                                    <label>Message</label>
-                                    <textarea class="form-control" name="msg" id="message" rows="5" placeholder="Message" required="required" ></textarea>
+                                <div class="form-group  controls mb-0 pb-2">
+                                    <label>Üzenet</label>
+                                    <textarea class="form-control" name="msg" id="message" rows="5" placeholder="Üzenet" required="required" ></textarea>
                                     <p class="help-block text-danger"></p>
                                 </div>
                            
